cfa879a0b4b24ca026644a6ff76bb96f49cc9820
Dumz3102 <drastra3102@gmail.com>
2024-10-30 14:40:17 +0700

    DungNM51: add Com.h, fix bugs in Rte_MirrorHeaterControl

--- ECU2/RTE/Rte_AppComTxRx.h
+++ ECU2/RTE/Rte_AppComTxRx.h
@@ -17,11 +17,7 @@ extern "C" {
 /* function prototype declarations                                            */
 /*----------------------------------------------------------------------------*/
 
-#define SIG_ID_HeaterControl 0x01
-#define SIG_ID_HeaterLevel   0x02
-
-extern FUNC(void, RTE_CODE) RTE_COMCbk_HeaterControl(void);
-extern FUNC(void, RTE_CODE) RTE_COMCbk_HeaterLevel(void);
+extern FUNC(void, RTE_CODE) Rte_COMCbk_HeaterControl(void);
 
 #ifdef __cplusplus
 } /* extern "C" */
