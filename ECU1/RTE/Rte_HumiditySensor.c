6b595f8fed4fca847e27ac964e44ff7348f9c63c
anhldptit2610 <anhldptit.2610@gmail.com>
2024-10-31 13:44:38 +0700

    AnhLD93: add commends to functions in Rte

--- ECU1/RTE/Rte_HumiditySensor.c
+++ ECU1/RTE/Rte_HumiditySensor.c
@@ -6,6 +6,15 @@ VAR(AUTOSAR_uint8, AUTOMATIC) HumidSensorData;
 
 extern FUNC(Std_ReturnType, AUTOMATIC) IoHwAb_HumiSensor_ReadData(VAR(HumidSensor, AUTOMATIC) id);
 
+/******************************************************************************/
+/* Name        : Rte_Call_HumiSensorIO_GetData                                */
+/* Param       : id - the ID of the temperature sensor                        */
+/*               data - pointer to the variable store the sensor data         */
+/* Return      :                                                              */
+/* Contents    : Call the IO hardware abstraction API to get the humidity     */
+/* Note        :                                                              */
+/******************************************************************************/
+
 FUNC(Std_ReturnType, RTE_CODE) Rte_Call_HumiSensorIO_GetData(VAR(HumiSensor, AUTOMATIC) id, P2VAR(AUTOSAR_uint8, AUTOMATIC, RTE_APPL_DATA) data)
 {
     VAR(Std_ReturnType, AUTOMATIC) ret = RTE_E_OK;
@@ -13,6 +22,14 @@ FUNC(Std_ReturnType, RTE_CODE) Rte_Call_HumiSensorIO_GetData(VAR(HumiSensor, AUT
     ret = IoHwAb_HumiSensor_ReadData(id, data);
     return ret;
 }
+
+/******************************************************************************/
+/* Name        : Rte_Write_PP_HumiData_HumiData                               */
+/* Param       : data - the data we want to write to the TempData's PPort     */
+/* Return      :                                                              */
+/* Contents    : write the data to the PPort in the Humidity SWC              */
+/* Note        :                                                              */
+/******************************************************************************/
 
 FUNC(Std_ReturnType, RTE_CODE) Rte_Write_PP_HumiData_HumiData(VAR(AUTOSAR_uint16, AUTOMATIC) data)
 {
@@ -23,7 +40,7 @@ FUNC(Std_ReturnType, RTE_CODE) Rte_Write_PP_HumiData_HumiData(VAR(AUTOSAR_uint16
 }
 
 /******************************************************************************/
-/* Name        : Rte_Call_HMRP_NvM_ReadHumidSensor                             */
+/* Name        : Rte_Call_HMRP_NvM_ReadHumidSensor                            */
 /* Param       : P2VAR: Pointer to the RAM data block                         */
 /* Return      :                                                              */
 /* Contents    : Read humidity calibration data from NvM SWC                  */
